2188ece76c6fb33d11f158c757a94f65f544a976
Dante Soares <dms3@rice.edu>
2014-04-22 14:08:10 -0500

    Updated specs

---
+++ spec/lib/openstax/api/representable_schema_printer_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+module OpenStax
+  module Api
+    describe RepresentableSchemaPrinter do
+      it 'must print model schemas' do
+        schema = RepresentableSchemaPrinter.json(DummyUserRepresenter)
+        expect(schema).to include('## Schema')
+        expect(schema).to include('{#')
+        expect(schema).to include(' .schema}')
+        expect(schema).to include("<pre class='code'>")
+        expect(schema).to include("{\n  \"type\": \"object\",\n  \"properties\": {\n    \"username\": {\n    },\n    \"password_hash\": {\n    }\n  },\n  \"required\": [\n\n  ],\n  \"definitions\": {\n  }\n}")
+        expect(schema).to include('</pre>')
+      end
+    end
+  end
+end
