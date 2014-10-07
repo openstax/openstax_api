6291b55fc2cb9adab871bf145c241b73e5d08aa8
Dante Soares <dms3@rice.edu>
2014-10-07 15:07:54 -0500

    Redid roar methods so that they can be used by more api calls; version bump

--- lib/openstax/api/roar.rb
+++ lib/openstax/api/roar.rb
@@ -8,43 +8,46 @@ module OpenStax
 
     module Roar
 
-      def get_representer(represent_with, model=nil)
-        return nil if represent_with.nil?
-        if represent_with.is_a? Proc
-          represent_with.call(model)
-        else
-          represent_with
+      def standard_create(model, represent_with=nil, &block)
+        standard_nested_create(model, nil, nil, represent_with, &block)
+      end
+
+      def standard_read(model, represent_with=nil)
+        OSU::AccessPolicy.require_action_allowed!(:read, current_api_user, @model)
+        respond_with model, represent_with: represent_with
+      end
+
+      def standard_update(model, represent_with=nil)
+        OSU::AccessPolicy.require_action_allowed!(:update, current_api_user, model)
+
+        model_klass.transaction do
+          consume!(model, represent_with: represent_with)
+          yield model if block_given?
+          OSU::AccessPolicy.require_action_allowed!(:update, current_api_user, model)
+        
+          if model.save
+            head :no_content
+          else
+            render json: model.errors, status: :unprocessable_entity
+          end
         end
       end
 
-      def standard_read(model_klass, id, represent_with=nil)
-        @model = model_klass.find(id)
-        OSU::AccessPolicy.require_action_allowed!(:read, current_api_user, @model)
-        respond_with @model, represent_with: get_representer(represent_with, @model)
-      end
-
-      def standard_update(model_klass, id, represent_with=nil)
-        @model = model_klass.find(id)
-        OSU::AccessPolicy.require_action_allowed!(:update, current_api_user, @model)
-        consume!(@model, represent_with: get_representer(represent_with, @model))
+      def standard_destroy(model)
+        OSU::AccessPolicy.require_action_allowed!(:destroy, current_api_user, model)
         
-        if @model.save
+        if model.destroy
           head :no_content
         else
-          render json: @model.errors, status: :unprocessable_entity
+          render json: model.errors, status: :unprocessable_entity
         end
       end
 
-      def standard_create(model_klass, represent_with=nil, &block)
-        standard_nested_create(model_klass, nil, nil, represent_with, &block)
-      end
-
-      def standard_nested_create(model_klass, container_association=nil, container_id=nil, represent_with=nil)
-        @model = model_klass.new()
-
-        if container_association && container_id
-          foreign_key = model_klass.reflect_on_association(container_association).association_foreign_key
-          @model.send(foreign_key + '=', container_id)
+      def standard_nested_create(model, container_association=nil,
+                                 container=nil, represent_with=nil)
+        if container_association && container
+          OSU::AccessPolicy.require_action_allowed!(:update, current_api_user, container)
+          model.send("#{container_association.to_s}=", container)
         end
 
         # Unlike the implications of the representable README, "consume!" can
@@ -52,80 +55,34 @@ module OpenStax
         # We do want to consume before checking the permissions so we can know
         # what we're dealing with, but if user doesn't have permission we don't
         # want to have changed the DB.  Wrap in a transaction to protect ourselves.
-
         model_klass.transaction do
-          consume!(@model, represent_with: get_representer(represent_with, @model))
-          yield @model if block_given?
-          OSU::AccessPolicy.require_action_allowed!(:create, current_api_user, @model)
-        end
+          consume!(model, represent_with: represent_with)
+          yield model if block_given?
+          OSU::AccessPolicy.require_action_allowed!(:create, current_api_user, model)
 
-        if @model.save
-          respond_with @model, represent_with: get_representer(represent_with, @model), status: :created
-        else
-          render json: @model.errors, status: :unprocessable_entity
-        end
-      end
-
-      def standard_destroy(model_klass, id)
-        @model = model_klass.find(id)
-        OSU::AccessPolicy.require_action_allowed!(:destroy, current_api_user, @model)
-        
-        if @model.destroy
-          head :no_content
-        else
-          render json: @model.errors, status: :unprocessable_entity
+          if model.save
+            respond_with model, represent_with: represent_with, status: :created
+          else
+            render json: model.errors, status: :unprocessable_entity
+          end
         end
       end
 
-      def standard_sort(model_klass)
-        # Take array of all IDs or hash of id => position,
-        # Regardless, build up an array of all IDs in the right order and pass those to sort
+      def standard_index(relation, represent_with)
+        model_klass = relation.base_class
+        OSU::AccessPolicy.require_action_allowed!(:index, current_api_user, model_klass)
+        respond_with relation, represent_with: represent_with
+      end
 
-        new_positions = params['newPositions']
-        return head :no_content if new_positions.length == 0
+      def standard_search(routine, query, options, represent_with)
+        model_klass = routine.send(:initial_relation).base_class
+        OSU::AccessPolicy.require_action_allowed!(:search, current_api_user, model_klass)
+        outputs = routine.call(query, options).outputs
+        respond_with outputs, represent_with: represent_with
+      end
 
-        # Can't have duplicate positions or IDs
-        unique_ids =       new_positions.collect{|np| np['id']}.uniq
-        unique_positions = new_positions.collect{|np| np['position']}.uniq
-
-        return head :bad_request if unique_ids.length != new_positions.length
-        return head :bad_request if unique_positions.length != new_positions.length
-
-        first = model_klass.where(:id => new_positions[0]['id']).first
-
-        return head :not_found if first.blank?
-
-        originalOrdered = first.me_and_peers.ordered.all
-
-        originalOrdered.each do |item|
-          raise SecurityTransgression unless item.send(:container_column) == originalOrdered[0].send(:container_column) \
-            if item.respond_to?(:container_column)
-          OSU::AccessPolicy.require_action_allowed!(:sort, current_api_user, item)
-        end
-
-        originalOrderedIds = originalOrdered.collect{|sc| sc.id}
-
-        newOrderedIds = Array.new(originalOrderedIds.size)
-      
-        new_positions.each do |newPosition|
-          id = newPosition['id'].to_i
-          newOrderedIds[newPosition['position']] = id
-          originalOrderedIds.delete(id)
-        end
-
-        ptr = 0
-        for oldId in originalOrderedIds 
-          while !newOrderedIds[ptr].nil?; ptr += 1; end
-          newOrderedIds[ptr] = oldId
-        end
-
-        begin 
-          model_klass.sort!(newOrderedIds)
-        rescue Exception => e
-          return head :internal_server_error
-        end
-
-        head :no_content
+      def standard_sort(*args)
+        raise NotYetImplemented
       end
       
     end
