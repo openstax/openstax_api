735bfc92450c753d43f18aa7b25d073e819eb17e
Dante Soares <dante.m.soares@rice.edu>
2021-01-26 18:01:53 -0600

    Rails 6 compatibility (Zeitwerk mode)

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   api :v1, default: true do
     get 'dummy', controller: 'dummy'
     get 'explode', controller: 'dummy'
