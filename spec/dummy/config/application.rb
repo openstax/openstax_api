0c5a748554c8d8a14ba8201a717321a9ce996f8c
Dante Soares <dante.m.soares@rice.edu>
2024-10-14 11:52:55 -0500

    Fix warnings in gem tests

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
 
     # Configuration for the application, engines, and railties goes here.
     #
